2cdbe05a53c00daee17e5d764a17aadb9580adcf
greggameplayer <gregoire.hage@gmail.com>
2020-02-23 05:38:51 +0100

    improve ladders & snakes positions when placing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import random
 import math
+import numpy as np
+
 objects = {
     'snakes': [],
     'ladders': [],
@@ -9,19 +11,21 @@ objects = {
     'startSnakes': [],
     'endSnakes': [],
     'graphicalLadders': [],
-    'different': 0,
-    'simtab': [9,8,7,6,5,4,3,2,1,0]
+    'differentladders': 0,
+    'differentsnakes': 0,
+    'simtab': [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 }
 
 
 def genLevel():
-    while objects['different'] == 0:
+    while objects['differentsnakes'] == 0 and objects['differentladders'] == 0:
+        objects['snakes'] = []
+        objects['ladders'] = []
         for i in range(random.randint(1, 4)):
             x = random.randint(0, 5)
             y = random.randint(0, 5)
             taille = random.randint(2, 4)
-            direction = random.randint(0, 1)
-            objects['snakes'].append([[abs(x + direction), y + j] for j in range(taille)])
+            objects['snakes'].append([[abs(x), y + j] for j in range(taille)])
             for j in range(len(objects['snakes'][i])):
                 if j % 2 == 1 and j != 0:
                     objects['snakes'][i][j][0] = objects['simtab'][objects['snakes'][i][j][0]]
@@ -29,25 +33,82 @@ def genLevel():
             x = random.randint(0, 5)
             y = random.randint(0, 5)
             taille = random.randint(2, 4)
-            direction = random.randint(0, 1)
-            objects['ladders'].append([[abs(x + direction), y+j] for j in range(taille)])
+            objects['ladders'].append([[abs(x), y + j] for j in range(taille)])
             for j in range(len(objects['ladders'][i])):
                 if j % 2 == 1 and j != 0:
                     objects['ladders'][i][j][0] = objects['simtab'][objects['ladders'][i][j][0]]
         print(objects)
         for elems in objects['ladders']:
             for elem in elems:
-                if elem.count(elem) > 1:
-                    objects['different'] = 0
+                if np.array_equal(np.isin(np.array(objects['ladders']), np.array(elem)), np.array([True, True])):
+                    objects['differentladders'] = 0
                 else:
-                    objects['different'] = 1
+                    print(np.array_equal(np.isin(np.array(objects['ladders']), np.sum([elem, [1, 1]], axis=0)),
+                                         np.array([True, True])))
+                    print(np.array_equal(np.isin(np.array(objects['ladders']), np.sum([elem, [0, 1]], axis=0)),
+                                         np.array([True, True])))
+                    print(np.array_equal(np.isin(np.array(objects['ladders']), np.sum([elem, [1, 0]], axis=0)),
+                                         np.array([True, True])))
+                    print(np.array_equal(np.isin(np.array(objects['ladders']), np.sum([elem, [-1, -1]], axis=0)),
+                                         np.array([True, True])))
+                    print(np.array_equal(np.isin(np.array(objects['ladders']), np.sum([elem, [0, -1]], axis=0)),
+                                         np.array([True, True])))
+                    print(np.array_equal(np.isin(np.array(objects['ladders']), np.sum([elem, [-1, 0]], axis=0)),
+                                         np.array([True, True])))
+                    if np.array(objects['ladders'])[
+                        np.isin(np.array(objects['ladders']), np.sum([[1,  - objects['simtab'][elem[1]] + 1], [1,  - objects['simtab'][elem[0]] + 1]], axis=0))].size == 0 and \
+                            np.array(objects['ladders'])[
+                                np.isin(np.array(objects['ladders']), np.sum([[1,  - objects['simtab'][elem[1]]], [1,  - objects['simtab'][elem[0]] + 1]], axis=0))].size == 0 and \
+                            np.array(objects['ladders'])[
+                                np.isin(np.array(objects['ladders']), np.sum([[1,  - objects['simtab'][elem[1]] + 1], [1,  - objects['simtab'][elem[0]]]], axis=0))].size == 0 \
+                            and np.array(objects['ladders'])[
+                            np.isin(np.array(objects['ladders']), np.sum([[1, - objects['simtab'][elem[1]] - 1], [1, - objects['simtab'][elem[0]] - 1]], axis=0))].size == 0 and \
+                                    np.array(objects['ladders'])[
+                            np.isin(np.array(objects['ladders']), np.sum([[1, - objects['simtab'][elem[1]]], [1, - objects['simtab'][elem[0]] - 1]], axis=0))].size == 0 and \
+                                    np.array(objects['ladders'])[
+                            np.isin(np.array(objects['ladders']), np.sum([[1, - objects['simtab'][elem[1]] - 1], [1, - objects['simtab'][elem[0]]]], axis=0))].size == 0 \
+                            and np.array(objects['ladders'])[
+                        np.isin(np.array(objects['ladders']), np.sum([elem, [0, 1]], axis=0))].size == 0 and \
+                            np.array(objects['ladders'])[
+                                np.isin(np.array(objects['ladders']), np.sum([elem, [1, 0]], axis=0))].size == 0 and \
+                            np.array(objects['ladders'])[
+                                np.isin(np.array(objects['ladders']), np.sum([elem, [1, 1]], axis=0))].size == 0:
+                        objects['differentladders'] = 1
 
         for elems in objects['snakes']:
             for elem in elems:
-                if elem.count(elem) > 1:
-                    objects['different'] = 0
-                else:
-                    objects['different'] = 1
+                if objects['differentsnakes'] != 1:
+                    if np.array_equal(np.isin(np.array(objects['snakes']), np.array(elem)), np.array([True, True])):
+                        objects['differentsnakes'] = 0
+                    else:
+                        if np.array(objects['snakes'])[
+                                np.isin(np.array(objects['snakes']), np.sum([elem, [1, 1]], axis=0))].size == 0 and \
+                                np.array(objects['snakes'])[
+                                    np.isin(np.array(objects['snakes']), np.sum([elem, [0, 1]], axis=0))].size == 0 and \
+                                np.array(objects['snakes'])[
+                                    np.isin(np.array(objects['snakes']), np.sum([elem, [1, 0]], axis=0))].size == 0 \
+                                and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [1, 1]], axis=0))].size != 0 and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [0, 1]], axis=0))].size != 0 and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [1, 0]], axis=0))].size != 0 \
+                                and np.array(objects['snakes'])[
+                                np.isin(np.array(objects['snakes']), np.sum([elem, [2, 2]], axis=0))].size == 0 and \
+                                np.array(objects['snakes'])[
+                                    np.isin(np.array(objects['snakes']), np.sum([elem, [2, 0]], axis=0))].size == 0 and \
+                                np.array(objects['snakes'])[
+                                    np.isin(np.array(objects['snakes']), np.sum([elem, [0, 2]], axis=0))].size == 0 \
+                                and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [2, 2]], axis=0))].size != 0 and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [0, 2]], axis=0))].size != 0 and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [2, 0]], axis=0))].size != 0 \
+                                and np.array(objects['snakes'])[
+                                np.isin(np.array(objects['snakes']), np.sum([elem, [1, 2]], axis=0))].size == 0 and \
+                                np.array(objects['snakes'])[
+                                    np.isin(np.array(objects['snakes']), np.sum([elem, [2, 1]], axis=0))].size == 0 \
+                                and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [1, 2]], axis=0))].size != 0 and np.array(elems)[
+                                np.isin(np.array(elems), np.sum([elem, [2, 1]], axis=0))].size != 0:
+                            objects['differentladders'] = 1
 
 
 def placeObjects():
@@ -56,7 +117,7 @@ def placeObjects():
         objects['endLadders'].append(objects['ladders'][i][len(objects['ladders'][i]) - 1])
         for j in range(len(objects['ladders'][i])):
             print(objects['ladders'][i][j][0], objects['ladders'][i][j][1])
-            Terrain.itemconfigure(carreau[objects['ladders'][i][j][1]][objects['ladders'][i][j][0]], fill="green")
+            Terrain.itemconfigure(carreau[objects['ladders'][i][j][1]][objects['ladders'][i][j][0]], fill="green", outline='grey')
     print(objects['startLadders'])
     print(objects['endLadders'])
 
@@ -66,7 +127,6 @@ def placeObjects():
         for j in range(len(objects['snakes'][i])):
             print(objects['snakes'][i][j][0], objects['snakes'][i][j][1])
             Terrain.itemconfigure(carreau[objects['snakes'][i][j][1]][objects['snakes'][i][j][0]], fill="red")
-
 
 
 fenetre = Tk()
