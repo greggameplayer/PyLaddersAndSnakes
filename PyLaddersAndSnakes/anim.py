6ee8a9ba34f5fbf7b8ea39952b8a69513f1c35e3
Restyled.io <commits@restyled.io>
2020-03-15 16:27:10 +0000

    Restyled by yapf

--- PyLaddersAndSnakes/anim.py
+++ PyLaddersAndSnakes/anim.py
@@ -7,14 +7,15 @@ from PyLaddersAndSnakes.functions import *
 
 class ANIM:
     def __init__(self, canvas, dice_face, rd):
-        wave_obj = sa.WaveObject.from_wave_file(find_data_file("sounds/dice.wav"))
+        wave_obj = sa.WaveObject.from_wave_file(
+            find_data_file("sounds/dice.wav"))
         play_obj_dice = wave_obj.play()
         for i in range(random.randint(8, 15)):
             canvas.Terrain.after(
                 100,
-                canvas.Terrain.itemconfigure(
-                    dice_face, text=random.randint(1, 6), fill="black"
-                ),
+                canvas.Terrain.itemconfigure(dice_face,
+                                             text=random.randint(1, 6),
+                                             fill="black"),
             )
         canvas.Terrain.itemconfigure(dice_face, text=rd, fill="red")
         canvas.Terrain.tag_unbind("dice_face", "<Button-1>")
@@ -25,12 +26,13 @@ class ANIM:
         canvas.detectWin()
         if canvas.playerturn == (len(canvas.player) - 1) and not canvas.win:
             canvas.playerturn = 0
-            canvas.Terrain.itemconfigure(
-                canvas.playerturnlabel, text="Au tour de " + str(canvas.PlayerNames[0])
-            )
+            canvas.Terrain.itemconfigure(canvas.playerturnlabel,
+                                         text="Au tour de " +
+                                         str(canvas.PlayerNames[0]))
         elif not canvas.win:
             canvas.playerturn += 1
             canvas.Terrain.itemconfigure(
                 canvas.playerturnlabel,
-                text=("Au tour de " + str(canvas.PlayerNames[canvas.playerturn])),
+                text=("Au tour de " +
+                      str(canvas.PlayerNames[canvas.playerturn])),
             )
